e0496cce57637ab3d801343b2fd04c2c03805bdc
peak2 <36886305+peak2@users.noreply.github.com>
2023-09-10 22:20:49 +0100

    pusih

--- dist/App.js
+++ dist/App.js
@@ -7,6 +7,7 @@ const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const config_1 = require("./config");
+const cors_1 = __importDefault(require("cors"));
 const morgan_1 = __importDefault(require("morgan"));
 const dbconfig_1 = __importDefault(require("./config/dbconfig"));
 const body_parser_1 = __importDefault(require("body-parser"));
@@ -16,16 +17,18 @@ const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 const { PORT } = dbconfig_1.default;
 dotenv_1.default.config();
-//to parse json body
 app.use((0, morgan_1.default)("dev"));
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
+app.use((0, cors_1.default)());
 app.use(body_parser_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
-app.use(express_1.default.static(path_1.default.join(__dirname, '../public')));
-config_1.db.sync({}).then(() => {
+app.use(express_1.default.static(path_1.default.join(__dirname, "../public")));
+config_1.db.sync({})
+    .then(() => {
     console.log("Database is connected");
-}).catch((err) => {
+})
+    .catch((err) => {
     console.log(err);
 });
 app.use("/users", userRoutes_1.default);
--- src/App.ts
+++ src/App.ts
@@ -1,45 +1,41 @@
-import  express  from "express";
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import {db} from "./config"
-import {HttpError} from 'http-errors';
-import cors from "cors"
-import logger from "morgan"
-import config from './config/dbconfig';
+import { db } from "./config";
+import { HttpError } from "http-errors";
+import cors from "cors";
+import logger from "morgan";
+import config from "./config/dbconfig";
 import bodyParser from "body-parser";
-import { UserInstance } from "./models/user";
-import UserRoutes from "./routes/userRoutes"
-import BlogRoutes from "./routes/blogRoutes"
-import path from 'path';
+import UserRoutes from "./routes/userRoutes";
+import BlogRoutes from "./routes/blogRoutes";
+import path from "path";
 
+const app = express();
+const { PORT } = config;
+dotenv.config();
 
+app.use(logger("dev"));
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "../public")));
 
-const app = express()
-const {PORT} = config
-dotenv.config()
-
-//to parse json body
-
-
-
-app.use(logger("dev"))
-app.use(express.json())
-app.use(cookieParser())
-app.use(bodyParser.json())
-app.use(express.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, '../public')));
-
-db.sync({}).then( ()=>{
+db.sync({})
+  .then(() => {
     console.log("Database is connected");
-}).catch((err:HttpError)=>{
+  })
+  .catch((err: HttpError) => {
     console.log(err);
-})
-app.use("/users", UserRoutes)
-app.use("/blogs", BlogRoutes)
-const BUILDPORT = PORT
+  });
+app.use("/users", UserRoutes);
+app.use("/blogs", BlogRoutes);
+const BUILDPORT = PORT;
 
-app.listen( BUILDPORT, ()=>{
-    console.log(`server running on port ${ BUILDPORT}`)
-})
+app.listen(BUILDPORT, () => {
+  console.log(`server running on port ${BUILDPORT}`);
+});
 
 export default app;
